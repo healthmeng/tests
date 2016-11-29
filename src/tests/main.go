9af4dee6c70b515a4efbd734e3c1a3fc1e86b6c8
tester <24625527@qq.com>
2016-11-29 16:53:47 +0800

    start parse  args

--- src/tests/main.go
+++ src/tests/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 "fmt"
+"os"
 )
 
+func prtUsage(){
+	fmt.Println("Usege:")
+}
+
+func tryCreate(){
+	fmt.Println("create:")
+}
+
+func listProj(){
+	fmt.Println("list:")
+}
+
+func tryEdit(){
+	fmt.Println("edit:")
+}
+
+func updateSrc(){
+	fmt.Println("update:")
+}
+
 func main(){
- fmt.Println("Start")
+	argc:=len(os.Args)
+	if argc<2{
+		prtUsage()
+	}else{
+		switch os.Args[1]{
+		case "create":
+			fallthrough
+		case "-c"
+			tryCreate()
+		case "list":
+			fallthrough
+		case "-l"
+			listProj()
+		case "list":
+			fallthrough
+		case "-l"
+			listProj()
+		}
+	}
 }
