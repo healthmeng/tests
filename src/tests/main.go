899b86bcd2998b1b142524b87ab933813b7dba57
root <root@dangdang-PC.lan>
2016-11-29 21:47:21 +0800

    add struct

--- src/tests/main.go
+++ src/tests/main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func prtUsage(){
+	fmt.Println("Tests is a tool for collecting your test codes and makes it easy for later review.")
 	fmt.Println("Usege:")
+	fmt.Println("\ttests command [args]")
+	fmt.Println("commands include:")
+	fmt.Println("\tcreate, -c dir|file: Create a test project.")
+	fmt.Println("\tlist, -l: List all test projects and their infomations(IDs may be most useful.")
+	fmt.Println("\tedit, -e proj_id: Edit an existing project.")
+	fmt.Println("\tupdate, -u proj_id [dir|file]: Update codes of an existing project.If path is not refered, try to use current dir.")
+	fmt.Println("\tdel, -d proj_id: Delete a project.")
 }
 
 func tryCreate(){
@@ -14,7 +22,6 @@ func tryCreate(){
 }
 
 func listProj(){
-	fmt.Println("list:")
 	if len(os.Args)>2{
 		prtUsage()
 	}else{
@@ -32,11 +39,11 @@ func updateSrc(){
 
 func delProj(){
     fmt.Println("del:")
-}   
+}
 
 func main(){
 	argc:=len(os.Args)
-	if argc<2 || argc>3{
+	if argc<2 || argc>4{
 		prtUsage()
 	}else{
 		switch os.Args[1]{
--- src/tests/op.go
+++ src/tests/op.go
@@ -4,6 +4,23 @@ import (
 "fmt"
 )
 
+var remotesvr string ="123.206.55.31"
+
+type PROJINFO struct{
+	id int
+	title string
+	atime string
+	descr string
+	conclude string
+	path string
+}
+
+
+
 func doList(){
-	fmt.Println("Do list")	
+	fmt.Println("Do list")
+}
+
+func doCreate(path string){
+	fmt.Println(path)
 }
