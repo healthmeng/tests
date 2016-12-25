0170a5e573f8353468d9f6af903b10790efa90f8
tester <24625527@qq.com>
2016-12-25 23:20:40 +0800

    fix search bug(keyword is not searched in datetime)

--- src/backend/backcore.go
+++ src/backend/backcore.go
@@ -190,7 +190,7 @@ func SearchProj(keywords []string)([]PROJINFO,error){
 		if index!=0{
 			query+=" AND "
 		}
-		query+=fmt.Sprintf("(title like '%%%s%%' OR descr like '%%%s%%' OR conclude like '%%%s%%' OR path like '%%%s%%' ",arg,arg,arg,arg)
+		query+=fmt.Sprintf("(title like '%%%s%%' OR descr like '%%%s%%' OR projtime like '%%%s%%' OR conclude like '%%%s%%' OR path like '%%%s%%' ",arg,arg,arg,arg,arg)
 		_,err:=strconv.Atoi(arg)
 		if err==nil{
 			query+=(" OR proj_id = "+arg)
