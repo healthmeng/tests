63a6eadab7f84ef5178588c04e7ad95a6960ee48
tester <24625527@qq.com>
2017-03-06 18:25:44 +0800

    start websvr

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 export GOPATH=`pwd`
 go get github.com/Go-SQL-Driver/MySQL 
-go install tserver tests
+go install tserver tests web
--- src/web/main.go
+++ src/web/main.go
@@ -1,4 +1,25 @@
 package main
 
+import(
+//"fmt"
+"html/template"
+"net/http"
+"log"
+)
+
+func listproj(w http.ResponseWriter, r *http.Request){
+	if r.Method=="GET"{
+		t,_:=template.ParseFiles("list.htm")
+//		tpl:=make(map[string] string)
+		t.Execute(w,nil)
+	}else{
+	}
+}
+
 func main(){
+	http.HandleFunc("/",listproj)
+	http.HandleFunc("/list",listproj)
+	if err:=http.ListenAndServe(":7777",nil);err!=nil{
+		log.Println("Error:",err)
+	}
 }
