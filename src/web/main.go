af3ae6a4bfd48c7f6480ecae95229f2a5fa354dc
tester <24625527@qq.com>
2017-03-07 17:03:43 +0800

    add project list in web

--- src/web/main.go
+++ src/web/main.go
@@ -2,16 +2,37 @@ package main
 
 import(
 //"fmt"
-"html/template"
+_"html/template"
+"io"
 "net/http"
 "log"
+"fmt"
+"backend"
 )
+
+func ListProjs(w io.Writer){
+	fmt.Fprintf(w,"<!DOCTYPE html>\n<head>\n<title> Project list </title>\n<meta charset=\"utf-8\" />\n<body>\n<table id=\"projtbl\" border=\"1\">\n")
+	projs,err:=backend.ListProj()
+
+	if err!=nil{
+		fmt.Fprintf(w,"%s\n",err.Error())
+	}else{
+		fmt.Fprintf(w,"<tr>\n<th>编号</th>\n<th>名称</th>\n<th>描述</th>\n<th>结论</th>\n<th>修改时间</th>\n</tr>\n")
+		for _,proj :=range projs{
+			fmt.Fprintf(w,"<tr>\n<td>%d</td>\n<td>%s</td>\n<td>%s</td>\n<td>%s</td>\n<td>%s</td>\n</tr>",
+				proj.Id,proj.Title,proj.Descr,proj.Conclude,proj.Atime)
+		}
+	}
+	fmt.Fprintf(w,"</table>\n</body>\n</head>\n")
+
+}
 
 func listproj(w http.ResponseWriter, r *http.Request){
 	if r.Method=="GET"{
-		t,_:=template.ParseFiles("list.htm")
+	//	t,_:=template.ParseFiles("list.htm")
 //		tpl:=make(map[string] string)
-		t.Execute(w,nil)
+	//	t.Execute(w,nil)
+		ListProjs(w)
 	}else{
 	}
 }
